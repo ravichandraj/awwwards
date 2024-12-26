aefee2b6028597a4c9327717a0c855e03f661f06
RaviChandra Jasthi <rc0437@gmail.com>
2024-12-26 17:32:44 +0530

    Added Button hover and github link for contact

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,17 @@
-const Button = ({title, id, leftIcon, rightIcon, containerClass}) => {
+
+const Button = ({title, id, leftIcon, rightIcon, containerClass , redirect}) => {
+
+    const handleRedirect = () => {
+        if (redirect) {
+            window.open(redirect, '_blank');
+        }
+    }
+
     return (
         <button id={id}
-                className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
+                onClick={handleRedirect}
+                className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black transition-colors duration-500 ease-in-out hover:bg-yellow-300 ${containerClass}`}>
             {leftIcon}
-
             <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
                 <div>{title}</div>
             </span>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
-import { FaDiscord, FaTwitter, FaYoutube, FaMedium } from "react-icons/fa";
+import {FaDiscord, FaTwitter, FaYoutube, FaMedium, FaGithub} from "react-icons/fa";
 
 const socialLinks = [
     { href: "https://discord.com", icon: <FaDiscord /> },
     { href: "https://twitter.com", icon: <FaTwitter /> },
     { href: "https://youtube.com", icon: <FaYoutube /> },
     { href: "https://medium.com", icon: <FaMedium /> },
+    { href:"https://github.com/ravichandraj/awwwards", icon: <FaGithub /> },
 ];
 
 const Footer = () => {
