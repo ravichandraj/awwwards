ac36fed22efe1bc5e8b90d22fef9217cdeb4811b
RaviChandra Jasthi <rc0437@gmail.com>
2024-12-26 17:51:03 +0530

    Fixed alignment for features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -73,7 +73,7 @@ const Features = () => {
                         world.
                     </p>
                 </div>
-                <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+                <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
                     <BentoCard
                         src="videos/feature-1.mp4"
                         title={
@@ -81,9 +81,9 @@ const Features = () => {
                         }
                         description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
                     />
-                </div>
+                </BentoTilt>
                 <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
-                    <BentoTilt className="bento-tilt_1 row-span-1">
+                    <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
                         <BentoCard
                             src="videos/feature-2.mp4"
                             title={<>zig<b>m</b>a</>}
